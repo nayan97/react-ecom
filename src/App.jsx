1abc3d93479c7d273ce60a39436a530799934bb8
nayan97 <nayanislam085@gmail.com>
2026-01-17 21:21:26 +0600

    created some pageses

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import Header from "./components/layouts/Header.jsx";
-import Footer from "./components/layouts/Footer.jsx";
+import { Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import Wishlist from './pages/Wishlist';
+import Checkout from './pages/Checkout';
+import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
   return (
     <>
-      <Header></Header>
-      <Footer></Footer>
+<Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+</Routes>
     </>
   );
 }
